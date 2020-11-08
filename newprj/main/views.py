62d2b8d659b85bd81667446b9e0aa2d27b5240b3
pverofeev <74044894+pverofeev@users.noreply.github.com>
2020-11-08 23:45:05 +0300

    08112020

--- newprj/main/views.py
+++ newprj/main/views.py
@@ -13,6 +13,8 @@ def index(request):
 def about(request):
     return render(request, 'main/about.html')
 
+def contacts(request):
+    return render(request, 'main/contacts.html')
 
 
 #def forum(request):
